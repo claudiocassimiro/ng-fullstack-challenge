6221d489da5bbe446f61cc721cbd722e4a5d5d26
claudiocassimiro <days_surfing@live.com>
2022-11-16 10:57:47 -0300

    feat: pass new import do appModule

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
+import { AccountsModule } from './modules/accounts/accounts.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AccountsModule],
   controllers: [],
   providers: [],
 })
