d89f9d741f33fbfe02f3e1d8be19a845a0fc344f
claudiocassimiro <days_surfing@live.com>
2022-11-16 09:45:09 -0300

    feat: implements bcrypt to hash the password and rafactor some lines

--- app/backend/src/modules/users/service/users.service.ts
+++ app/backend/src/modules/users/service/users.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { PrismaService } from 'src/shared/database/prisma.service';
+import { BcryptService } from 'src/shared/hash/BcryptService';
 import { UserDTO } from '../dto/users.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private readonly bcrypt: BcryptService,
+  ) {}
 
   async create(data: UserDTO) {
+    const { username, password } = data;
+
     const verifyIfUserExists = await this.prisma.user.findFirst({
       where: {
-        username: data.username,
+        username,
       },
     });
 
@@ -18,13 +24,15 @@ export class UsersService {
       throw new Error('User already exists');
     }
 
+    const hashedPassword = await this.bcrypt.hash(password);
+
     const id = randomUUID();
 
     const user = await this.prisma.user.create({
       data: {
         id,
-        username: data.username,
-        password: data.password,
+        username,
+        password: hashedPassword,
       },
     });
 
