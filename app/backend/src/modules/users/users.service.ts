44e753029c3bb19e8e2f412c40e257bc4e825cb3
claudiocassimiro <days_surfing@live.com>
2022-11-16 09:24:53 -0300

    feat: implements service to create user

--- app/backend/src/modules/users/users.service.ts
+++ app/backend/src/modules/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { UserDTO } from './users.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: UserDTO) {
+    const verifyIfUserExists = await this.prisma.user.findFirst({
+      where: {
+        username: data.username,
+      },
+    });
+
+    if (verifyIfUserExists) {
+      throw new Error('Book already exists');
+    }
+
+    const user = await this.prisma.user.create({
+      data,
+    });
+
+    return user;
+  }
+}
