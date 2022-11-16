93e2604b28a26bde895e26038205db5950f7eee8
claudiocassimiro <days_surfing@live.com>
2022-11-16 12:02:01 -0300

    feat: implements login service router

--- app/backend/src/modules/users/service/users.service.ts
+++ app/backend/src/modules/users/service/users.service.ts
@@ -4,6 +4,7 @@ import { AccountsService } from 'src/modules/accounts/service/accounts.service';
 import { PrismaService } from 'src/shared/database/prisma.service';
 import { BcryptService } from 'src/shared/hash/BcryptService';
 import { UserDTO } from '../dto/users.dto';
+import { JWTService } from 'src/shared/jwt/JWTService';
 
 @Injectable()
 export class UsersService {
@@ -11,9 +12,10 @@ export class UsersService {
     private prisma: PrismaService,
     private readonly bcrypt: BcryptService,
     private account: AccountsService,
+    private jwt: JWTService,
   ) {}
 
-  async create(data: UserDTO) {
+  async create(data: UserDTO): Promise<UserDTO | Error> {
     const { username, password } = data;
 
     const verifyIfUserExists = await this.prisma.user.findFirst({
@@ -48,5 +50,27 @@ export class UsersService {
     this.account.create(userAccount);
 
     return { ...user, accountId };
+  }
+
+  async login({ username, password }: UserDTO): Promise<string | Error> {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      throw new Error('User not exists');
+    }
+
+    const passwordMatch = await this.bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Invalid password');
+    }
+
+    const jwtToken = this.jwt.sign(user.id);
+
+    return jwtToken;
   }
 }
