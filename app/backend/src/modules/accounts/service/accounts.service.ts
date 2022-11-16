60d56bab036e5d3cc0cc932ab7b2de7f4b9ea872
claudiocassimiro <days_surfing@live.com>
2022-11-16 11:48:26 -0300

    feat: implements router to create account

--- app/backend/src/modules/accounts/service/accounts.service.ts
+++ app/backend/src/modules/accounts/service/accounts.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { accountDTO } from '../dto/accounts.dto';
+import { PrismaService } from 'src/shared/database/prisma.service';
 
 @Injectable()
-export class AccountsService {}
+export class AccountsService {
+  constructor(private prisma: PrismaService) {}
+  async create(data: accountDTO) {
+    const { userId } = data;
+
+    const verifyIfUserExists = await this.prisma.user.findFirst({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!verifyIfUserExists) {
+      throw new Error('User not exists on database');
+    }
+
+    return this.prisma.account.create({
+      data,
+    });
+  }
+}
