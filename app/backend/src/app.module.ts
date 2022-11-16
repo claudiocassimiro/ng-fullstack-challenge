da97cba35d9a807f407a18d512a53bfd824b14d7
claudiocassimiro <days_surfing@live.com>
2022-11-16 19:48:44 -0300

    feat: implements middleware to accounts router method post

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AccountsModule } from './modules/accounts/accounts.module';
+import { ApiTokenCheckMiddleware } from './shared/middlewares/api-token-check.middleware';
 
 @Module({
   imports: [UsersModule, AccountsModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ApiTokenCheckMiddleware)
+      .forRoutes({ path: 'accounts', method: RequestMethod.POST });
+  }
+}
