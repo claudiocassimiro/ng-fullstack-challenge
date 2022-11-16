4ea7ee93233a74d581a2528e503b3cdea6ad3f52
claudiocassimiro <days_surfing@live.com>
2022-11-16 10:33:06 -0300

    feat: implements Post router to create a user

--- app/backend/src/modules/users/controller/users.controller.ts
+++ app/backend/src/modules/users/controller/users.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { validatePassword } from 'src/shared/helpers/validatePassword';
+import { UserDTO } from '../dto/users.dto';
 import { UsersService } from '../service/users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() data: UserDTO) {
+    const { username, password } = data;
+
+    if (username.length < 3) {
+      throw new Error('username too short');
+    }
+
+    if (!validatePassword(password)) {
+      throw new Error('password too weak');
+    }
+
+    return this.usersService.create(data);
+  }
 }
