0f79db4002a5a4678c1cfe159568e7db8e78c005
claudiocassimiro <days_surfing@live.com>
2022-11-17 10:33:11 -0300

    feat: pass all neededs providers

--- app/backend/src/modules/accounts/accounts.module.ts
+++ app/backend/src/modules/accounts/accounts.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AccountsService } from './service/accounts.service';
 import { AccountsController } from './controller/accounts.controller';
 import { PrismaService } from 'src/shared/database/prisma.service';
+import { UsersService } from '../users/service/users.service';
+import { BcryptService } from 'src/shared/hash/BcryptService';
+import { JWTService } from 'src/shared/jwt/JWTService';
 
 @Module({
   controllers: [AccountsController],
-  providers: [AccountsService, PrismaService],
+  providers: [
+    AccountsService,
+    PrismaService,
+    UsersService,
+    BcryptService,
+    JWTService,
+  ],
 })
 export class AccountsModule {}
