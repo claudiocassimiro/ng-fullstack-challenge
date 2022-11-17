9ec113efca46a276d8a174f8c40c3cf41fcd78db
claudiocassimiro <days_surfing@live.com>
2022-11-17 10:32:53 -0300

    feat: implements post router to make a cashout

--- app/backend/src/modules/accounts/controller/accounts.controller.ts
+++ app/backend/src/modules/accounts/controller/accounts.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UserDTO } from 'src/modules/users/dto/users.dto';
 import { AccountsService } from '../service/accounts.service';
+import { CashOutType } from '../types';
 
 @Controller('accounts')
 export class AccountsController {
@@ -8,6 +9,25 @@ export class AccountsController {
 
   @Post('/balance')
   async getBalance(@Body() { accountId }: UserDTO) {
+    if (!accountId) {
+      throw new Error('Please send the correct values');
+    }
+
     return this.accountsService.getBalance(accountId);
+  }
+
+  @Post('/cashout')
+  async cashOut(@Body() cashOutObject: CashOutType) {
+    const { cashOutUsername, cashInUsername, cashOutAccountId, balance } =
+      cashOutObject;
+    if (!cashOutUsername || !cashInUsername || !cashOutAccountId || !balance) {
+      throw new Error('Please send the correct values');
+    }
+
+    if (cashOutUsername === cashInUsername) {
+      throw new Error('User cannot transfer to himself');
+    }
+
+    return this.accountsService.setCashOut(cashOutObject);
   }
 }
