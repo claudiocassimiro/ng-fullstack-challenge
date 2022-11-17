8c98ecfcdf233a96e77dbd76b24f92e1de9e26e8
claudiocassimiro <days_surfing@live.com>
2022-11-17 17:23:55 -0300

    feat: add httpException to throw frendily errors to front

--- app/backend/src/shared/hash/BcryptService.ts
+++ app/backend/src/shared/hash/BcryptService.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 
 @Injectable()
@@ -9,7 +9,7 @@ export class BcryptService {
     try {
       return await bcrypt.hash(password, 10);
     } catch (error) {
-      throw new Error(error);
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
@@ -17,7 +17,7 @@ export class BcryptService {
     try {
       return bcrypt.compare(password, hash);
     } catch (error) {
-      throw new Error(error);
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
