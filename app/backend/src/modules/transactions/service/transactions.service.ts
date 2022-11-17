02d9f82b559e9eceddb040daad2dee1175ad25a1
claudiocassimiro <days_surfing@live.com>
2022-11-17 16:50:01 -0300

    feat: implements services router to get transactions

--- app/backend/src/modules/transactions/service/transactions.service.ts
+++ app/backend/src/modules/transactions/service/transactions.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/shared/database/prisma.service';
 import { randomUUID } from 'crypto';
 import { TransactionsDTO } from '../dto/transactions.dto';
+import { TransactionsByDate, TransactionsByType } from '../types';
 
 @Injectable()
 export class TransactionsService {
@@ -12,6 +13,88 @@ export class TransactionsService {
       const id = randomUUID();
 
       return this.prisma.transaction.create({ data: { id, ...data } });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async findAllTransactions(accountId: string) {
+    try {
+      const transactions = await this.prisma.transaction.findMany({
+        where: {
+          OR: [
+            { debitedAccountId: accountId },
+            { creditedAccountId: accountId },
+          ],
+        },
+      });
+
+      return transactions;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async findAllTransactionsByDate({ accountId, date }: TransactionsByDate) {
+    try {
+      const formatedDate = new Date(date).toISOString();
+
+      const transactions = await this.prisma.transaction.findMany({
+        where: {
+          OR: [
+            { debitedAccountId: accountId },
+            { creditedAccountId: accountId },
+          ],
+          AND: {
+            createdAt: formatedDate,
+          },
+        },
+      });
+
+      const cashOutTransactions = [];
+      const cashInTransactions = [];
+
+      transactions.forEach((transaction) => {
+        if (transaction.debitedAccountId === accountId) {
+          cashOutTransactions.push({ type: 'cashOut', ...transaction });
+        } else {
+          cashInTransactions.push({ type: 'cashIn', ...transaction });
+        }
+      });
+
+      return [...cashOutTransactions, ...cashInTransactions];
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async findAllTransactionsByType({ accountId, type }: TransactionsByType) {
+    try {
+      const transactions = await this.prisma.transaction.findMany({
+        where: {
+          OR: [
+            { debitedAccountId: accountId },
+            { creditedAccountId: accountId },
+          ],
+        },
+      });
+
+      const cashOutTransactions = [];
+      const cashInTransactions = [];
+
+      transactions.forEach((transaction) => {
+        if (transaction.debitedAccountId === accountId) {
+          cashOutTransactions.push({ type: 'cashOut', ...transaction });
+        } else {
+          cashInTransactions.push({ type: 'cashIn', ...transaction });
+        }
+      });
+
+      if (type === 'cashOut') {
+        return cashOutTransactions;
+      }
+
+      return cashInTransactions;
     } catch (error) {
       throw new Error(error);
     }
