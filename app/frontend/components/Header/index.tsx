cde7964fac7d157ec8cafb6a5adf46ddf4d577d3
claudiocassimiro <days_surfing@live.com>
2022-11-20 09:43:05 -0300

    feat: implements component header

---
+++ app/frontend/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { useWindowSize } from "../../utils/helpers";
+import styles from "./styles.module.css";
+
+export default function Header() {
+  const { isDesktop } = useWindowSize();
+  return (
+    <header className={styles.header}>
+      <Image
+        src="/images/logo-ngcash.svg"
+        alt="logo ngcash"
+        width={isDesktop ? 160 : 120}
+        height={isDesktop ? 48 : 38}
+      />
+      <div className={styles.HeaderContainerButtons}>
+        <a className={styles.HeaderButton} href="#">
+          Login
+        </a>
+        <a className={styles.HeaderButton} href="#">
+          Criar uma conta
+        </a>
+      </div>
+    </header>
+  );
+}
