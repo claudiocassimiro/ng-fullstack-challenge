f85e0efe4c9c0d6986da6da28b476aecacfb59b0
claudiocassimiro <days_surfing@live.com>
2022-11-20 08:51:46 -0300

    feat: implements home page without header

--- app/frontend/pages/index.tsx
+++ app/frontend/pages/index.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container} />;
+  return (
+    <div className={styles.HomeContainer}>
+      {/* <Header /> */}
+      <main className={styles.HomeMainContent}>
+        <Image
+          src="/images/ngcash.webp"
+          alt="logo da ng.cash"
+          width="556"
+          height="406"
+        />
+        <div className={styles.HomeContainerText}>
+          <p className={styles.HomeAboutUsText}>
+            Somos a carteira digital da Nova Geração.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
 }
