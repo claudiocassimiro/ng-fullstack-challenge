195406cccf078e7f7ad2510153028d4fff52855a
claudiocassimiro <days_surfing@live.com>
2022-11-21 15:43:02 -0300

    feat: implements logic to possibility transactions

--- app/frontend/pages/account.tsx
+++ app/frontend/pages/account.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import styles from "../styles/Account.module.css";
 import { useWindowSize } from "../utils/helpers";
@@ -14,6 +14,11 @@ export default function Account() {
 
   const [userObject, setUserObject] = useState(initialUserObject);
   const [token, setToken] = useState("");
+  const [transactionUsername, setTransactionUsername] = useState("");
+  const [transactionValue, setTransactionValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [sucessMessage, setSucessMessage] = useState("");
+  const [formattedBalance, setFormatedBalance] = useState("");
   const [balance, setBalance] = useState(0);
   const { isDesktop } = useWindowSize();
   const [tokenCookie, , removeTokenCookie] = useCookies(["token"]);
@@ -59,7 +64,8 @@ export default function Account() {
         currency: "BRL",
       });
 
-      setBalance(formatedBalance);
+      setBalance(data.balance);
+      setFormatedBalance(formatedBalance);
     } catch (error) {
       console.log(error);
     }
@@ -68,6 +74,66 @@ export default function Account() {
   useEffect(() => {
     getAccountBalance();
   }, [token]);
+
+  const handleTransactionSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (transactionUsername === username) {
+      return setErrorMessage(
+        "Você não pode fazer uma transação para você mesmo."
+      );
+    }
+
+    if (Number(transactionValue) > balance) {
+      return setErrorMessage("Saldo insuficiente.");
+    }
+
+    const cashoutObject = {
+      cashOutUsername: username,
+      cashOutAccountId: accountId,
+      cashInUsername: transactionUsername,
+      balance: Number(transactionValue),
+    };
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/accounts/cashout`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            token: token,
+          },
+          body: JSON.stringify(cashoutObject),
+        }
+      );
+
+      const data = await response.json();
+
+      setSucessMessage(data.message);
+      setTransactionUsername("");
+      setTransactionValue("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (sucessMessage.length > 1) {
+      getAccountBalance();
+      setTimeout(() => {
+        setSucessMessage("");
+      }, 3000);
+    }
+  }, [sucessMessage]);
+
+  useEffect(() => {
+    if (errorMessage.length > 1) {
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 3000);
+    }
+  }, [errorMessage]);
 
   return (
     <div className={styles.AccountContainer}>
@@ -88,12 +154,50 @@ export default function Account() {
       </header>
       <main className={styles.AccountMainContainer}>
         <div className={styles.AccountDataContainer}>
-          <h1
-            className={styles.AccountGreeatingMessage}
-          >{`Olá, ${username}!`}</h1>
+          <p className={styles.AccountText}>{`Olá, ${username}!`}</p>
           <div className={styles.AccountBalanceDisplay}>
-            <span className={styles.AccountBalance}>{balance}</span>
+            <span className={styles.AccountBalance}>{formattedBalance}</span>
           </div>
+        </div>
+        <div className={styles.AccountDataContainer}>
+          <p className={styles.AccountText}>{`Área de transferências`}</p>
+          <form
+            onSubmit={(e) => handleTransactionSubmit(e)}
+            className={styles.AccountTransactionContainer}
+          >
+            <div className={styles.AccountContainerInputs}>
+              <input
+                required
+                className={styles.AccountTransactionInputUsername}
+                placeholder="Quem vai receber?"
+                type="text"
+                name="transactionUsername"
+                value={transactionUsername}
+                onChange={(e) => setTransactionUsername(e.target.value)}
+              />
+              <input
+                required
+                className={styles.AccountTransactionInputValue}
+                placeholder="Valor"
+                type="number"
+                name="transactionValue"
+                value={transactionValue}
+                onChange={(e) => setTransactionValue(e.target.value)}
+              />
+            </div>
+            {errorMessage.length > 0 ? (
+              <p className={styles.AccountErrorMessage}>{errorMessage}</p>
+            ) : null}
+            {sucessMessage.length > 0 ? (
+              <p className={styles.AccountSucessMessage}>{sucessMessage}</p>
+            ) : null}
+            <button
+              className={styles.AccountSubmitTransactionButton}
+              type="submit"
+            >
+              Enviar
+            </button>
+          </form>
         </div>
       </main>
     </div>
