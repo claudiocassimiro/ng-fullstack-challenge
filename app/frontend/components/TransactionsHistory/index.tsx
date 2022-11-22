2f2bef291f3f075c49a8f03182709b14b2d65f9b
claudiocassimiro <days_surfing@live.com>
2022-11-22 15:06:25 -0300

    feat: implements transaction history

--- app/frontend/components/TransactionsHistory/index.tsx
+++ app/frontend/components/TransactionsHistory/index.tsx
@@ -7,14 +7,18 @@ import styles from "./styles.module.css";
 interface TransactionsHistoryProps {
   accountId: string;
   token: string;
+  showHistory: boolean;
+  setShowHistory: (value: boolean) => void;
 }
 
 export default function TransactionsHistory({
   accountId,
   token,
+  showHistory,
+  setShowHistory,
 }: TransactionsHistoryProps) {
-  const [transactions, setTransactions] = useState<Transactions[]>();
-  const [showHistory, setShowHistory] = useState(false);
+  const [transactions, setTransactions] = useState<Transactions[]>([]);
+
   const getAllTransactions = async () => {
     try {
       const response = await fetch(
@@ -36,6 +40,21 @@ export default function TransactionsHistory({
     }
   };
 
+  const handleAllTransactions = () => {
+    setShowHistory(!showHistory);
+    getAllTransactions();
+  };
+
+  const formatDate = (date: Date) => {
+    const dateFormated = new Date(date).toISOString();
+
+    const day = dateFormated.split("T")[0].split("-")[2];
+    const month = dateFormated.split("T")[0].split("-")[1];
+    const year = dateFormated.split("T")[0].split("-")[0];
+
+    return `${day}/${month}/${year}`;
+  };
+
   return (
     <div className={styles.TransactionsContainer}>
       {!showHistory ? (
@@ -43,7 +62,7 @@ export default function TransactionsHistory({
           <button
             className={styles.TransactionsButton}
             type="button"
-            onClick={getAllTransactions}
+            onClick={handleAllTransactions}
           >
             Ver todas as transações
           </button>
@@ -63,7 +82,38 @@ export default function TransactionsHistory({
           />
         </div>
       ) : (
-        <h1 style={{ color: "white" }}>{transactions?.length}</h1>
+        <div className={styles.TransactionsHistoryContainer}>
+          {transactions?.map((transaction) => {
+            return (
+              <div
+                className={styles.TransactionsContainerTransaction}
+                key={transaction.id}
+              >
+                <div className={styles.TransactionsContainerInfos}>
+                  <span className={styles.TransactionsType}>
+                    {transaction.type === "cashOut"
+                      ? "Transferência enviada"
+                      : "Transferência recebida"}
+                  </span>
+                  <span className={styles.TransactionsUsername}>
+                    {transaction?.username}
+                  </span>
+                  <span className={styles.TransactionsValue}>
+                    {transaction.value.toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+                  </span>
+                </div>
+                <div>
+                  <span className={styles.TransactionsDate}>
+                    {formatDate(transaction.createdAt)}
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       )}
     </div>
   );
