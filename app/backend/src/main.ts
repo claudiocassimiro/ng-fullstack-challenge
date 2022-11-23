b1a42d8981cffda659b1eee77c1cc9435f9caf84
claudiocassimiro <days_surfing@live.com>
2022-11-22 22:19:40 -0300

    feat: try to fix cors

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  app.enableCors(options);
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
